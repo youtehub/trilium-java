f9e7015db50a33d9b54dd62f9975c4139bb744d7
wy1994 <2728142607@qq.com>
2022-09-04 22:35:41 +0800

    init

---
+++ src/main/java/com/md/view/StartApplication.java
@@ -0,0 +1,15 @@
+package com.md.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
